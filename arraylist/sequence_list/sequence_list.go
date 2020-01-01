6921e02e295e744924e72325a0c93fc7f993c9df
jihonghe <1654508502@qq.com>
2020-01-02 01:54:09 +0800

    Add: DeleteLast(), Delete(), Clear(), Set(), Get(), Index(), Contains(), Traverse(); Add the new functions test and finished all tests.

--- arraylist/sequence_list/sequence_list.go
+++ arraylist/sequence_list/sequence_list.go
@@ -1,5 +1,10 @@
 package sequence_list
 
+import (
+	"errors"
+	"fmt"
+)
+
 type SequenceList struct {
 	// 通过切片存储元素值
 	elements []int
@@ -66,18 +71,133 @@ func (l *SequenceList) Insert(elementValue int, index int) bool {
 	}
 
 	l.elements = append(l.elements, elementValue)
-	for leftIndex := l.length; leftIndex < index; {
-		l.elements[leftIndex - 1], l.elements[leftIndex] = l.elements[leftIndex], l.elements[leftIndex - 1]
-		leftIndex--
-	}
+	copy(l.elements[index + 1:], l.elements[index:l.length])
+	l.elements[index] = elementValue
 	l.length++
 
 	return true
 }
 
+// DeleteLast: 删除表尾的一个元素
+func (l *SequenceList) DeleteLast() bool {
+	if l.IsEmpty() {
+		println("DeleteLast failed: the list is empty.")
+		return false
+	}
+
+	l.elements = l.elements[:l.length-1]
+	l.length--
+
+	return true
+}
+
+// Delete: 删除表中指定位置的元素
+func (l *SequenceList) Delete(index int) bool {
+	if l.IsEmpty() {
+		println("Delete failed: the list is empty.")
+		return false
+	}
+
+	if index < 0 || index >= l.length {
+		println("Delete failed: the given index is invalid.")
+		return false
+	}
+
+	copy(l.elements[index:], l.elements[index + 1:])
+	l.elements = l.elements[:l.length - 1]
+	l.length--
+
+	return true
+}
+
+// Clear: 清除表中的所有元素
+func (l *SequenceList) Clear() bool {
+	if l.IsEmpty() {
+		return true
+	}
+
+	l.elements = l.elements[:0]
+	l.length = 0
+
+	return true
+}
+
+// Set: 修改指定位置的元素的值
+func (l *SequenceList) Set(elementValue int, index int) bool {
+	if l.IsEmpty() {
+		println("Set failed: the list is empty.")
+		return false
+	}
+	if index < 0 || index >= l.length {
+		println("Set failed: the given index is invalid.")
+		return false
+	}
+
+	l.elements[index] = elementValue
+
+	return true
+}
+
+// Get: 获取指定位置的元素值
+func (l SequenceList) Get(index int) (int, error) {
+	if l.IsEmpty() {
+		return 0, errors.New("Get failed: the list is empty")
+	}
+
+	if index < 0 || index >= l.length {
+		return 0, errors.New("Get failed: the given index is invalid")
+	}
+
+	return l.elements[index], nil
+}
+
+// Index: 获取给定值在表中第一次出现的位置
+func (l SequenceList) Index(elementValue int) int {
+	if l.IsEmpty() {
+		return -1
+	}
+
+	for index, element := range l.elements {
+		if elementValue == element {
+			return index
+		}
+	}
+
+	return -1
+}
+
+// Contains: 判断给出的元素值是否在表中
+func (l SequenceList) Contains(elementValue int) bool {
+	if l.IsEmpty() {
+		return false
+	}
+
+	for _, element := range l.elements {
+		if element == elementValue {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Traverse: 遍历顺序表元素
+func (l SequenceList) Traverse() {
+	if l.IsEmpty() {
+		return
+	}
+
+	print("Sequence list: ")
+	elementsListString := "["
+	for _, element := range l.elements {
+		elementsListString += fmt.Sprintf("%d, ", element)
+	}
+	elementsListString += "]"
+}
+
 // IsEmpty: 判断表是否为空
 func (l SequenceList) IsEmpty() bool {
-	return l.length == 0
+	return (l.length == len(l.elements)) && (len(l.elements) == 0)
 }
 
 // IsFull: 判断表是否已满
--- arraylist/sequence_list/sequence_list_test.go
+++ arraylist/sequence_list/sequence_list_test.go
@@ -82,3 +82,165 @@ func TestSequenceList_Insert(t *testing.T) {
 	}
 	println()
 }
+
+func TestSequenceList_DeleteLast(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能否正确处理
+	// 2. 表不为空时，元素是否正确删除，表的长度是否正确
+
+	list := New()
+	deleted := list.DeleteLast()
+	if deleted {
+		t.Error("DeleteLast() Error: the empty list should not finish delete.")
+	}
+
+	list.Append(4)
+	list.Append(3)
+	list.Append(3)
+	deleted = list.DeleteLast()
+	if !deleted || len(list.elements) != 2 {
+		t.Error("DeleteLast() Error: the length of list who has only one element after function DeleteLast() is not equal 0.")
+	}
+}
+
+func TestSequenceList_Delete(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能否正确处理
+	// 2. 表不为空时，能否正确处理边界条件：删除首尾元素
+	// 3. 表不为空时，能否正确处理删除非首尾元素
+
+	list := New()
+	deleted := list.Delete(3)
+	if deleted {
+		t.Error("Delete() Error: empty list cannot delete element.")
+	}
+
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	list.Append(6)
+	list.Append(7)
+	deleted = list.Delete(0)
+	if !deleted || len(list.elements) != 4 {
+		t.Error("Delete() Error: the length of list is error after deleting an element.")
+	}
+
+	list.Delete(3)
+	if !deleted || len(list.elements) != 3 {
+		t.Error("Delete() Error: the length of list is error after deleting an element.")
+	}
+	list.Delete(1)
+	if !deleted || len(list.elements) != 2 {
+		t.Error("Delete() Error: the length of list is error after deleting an element.")
+	}
+}
+
+func TestSequenceList_Clear(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时
+	// 2. 表不为空时，经过Clear()后表长度是否为0
+
+	list := New()
+	cleared := list.Clear()
+	if !cleared {
+		t.Error("Clear() Error: empty list clear should return true.")
+	}
+	list.Append(9)
+	list.Append(9)
+	list.Append(9)
+	list.Append(9)
+	cleared = list.Clear()
+	if !cleared {
+		t.Error("Clear() Error: the list with elements dose not clear successfully.")
+	}
+}
+
+func TestSequenceList_Set(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能否正确处理
+	// 2. 表不为空时，能否正确修改对应位置的元素
+
+	list := New()
+	updated := list.Set(3, 1)
+	if updated {
+		t.Error("Set() Error: empty list should not return true.")
+	}
+
+	list.Append(3)
+	list.Append(3)
+	list.Append(3)
+	updated = list.Set(9, 1)
+	if !updated || list.elements[1] != 9 {
+		t.Error("Set Error: Set() failed.")
+	}
+}
+
+func TestSequenceList_Get(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能否正确处理
+	// 2. 表不为空时，若所给的index不合理能否正确处理，若index合理，校验返回值与在表中的实际值是否一致
+
+	list := New()
+	value, err := list.Get(3)
+	if err == nil {
+		t.Error("Get() Error: empty list should returns error.")
+	}
+
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	value, err = list.Get(5)
+	if err == nil {
+		t.Error("Get() Error: invalid index should returns error.")
+	}
+
+	value, err = list.Get(1)
+	if err != nil {
+		t.Error("Get() Error: the index is valid, it should not return error.")
+	}
+	if value != list.elements[1] {
+		t.Error("Get() Error: the returned value is not equal to the element`s value in the list.")
+	}
+}
+
+func TestSequenceList_Index(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时，能否正确处理
+	// 2. 表不为空时，查找不在表中的元素能否正确返回-1
+	// 3. 表不为空时，查找在表中的元素能否正确返回相应的index
+
+	list := New()
+	index := list.Index(9)
+	if index != -1 {
+		t.Error("Index() Error: empty list should return -1")
+	}
+
+	list02 := New(3, 1, 4, 5, 0, 1)
+	index = list02.Index(9)
+	if index != -1 {
+		t.Error("Index() Error: 9 is not in list, Index() should return -1")
+	}
+
+	index = list02.Index(1)
+	if index != 1 {
+		t.Error("Index() Error: the returned index is not equal to the index that the element in list.")
+	}
+}
+
+func TestSequenceList_Contains(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能否正确处理
+	// 2. 表不为空时，所得结果是否与实际相符
+
+	list := New()
+	in := list.Contains(3)
+	if in {
+		t.Error("Contains() Error: empty list should return false")
+	}
+
+	list02 := New(3, 1, 9)
+	in = list02.Contains(0)
+	if in {
+		t.Error("Contains() Error: element 0 is not in the list")
+	}
+}
