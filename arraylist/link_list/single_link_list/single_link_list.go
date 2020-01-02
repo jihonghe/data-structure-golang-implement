770805b180985eac1bc18c6137a65a7941ca9934
jihonghe <1654508502@qq.com>
2020-01-03 01:01:41 +0800

    实现单链表的创建、遍历、判空、添加元素等操作函数

--- arraylist/link_list/single_link_list/single_link_list.go
+++ arraylist/link_list/single_link_list/single_link_list.go
@@ -1,1 +1,147 @@
 package single_link_list
+
+import "fmt"
+
+type element struct {
+	value interface{}
+	next *element
+}
+
+type SingleLinkList struct {
+	first *element
+	last *element
+	length int
+}
+
+// NewElement: 创建新节点
+func NewElement(elementValue interface{}) *element {
+	return &element{
+		value: elementValue,
+		next: nil,
+	}
+}
+
+// New: 创建并初始化单链表
+func New(elements ...interface{}) *SingleLinkList {
+	if len(elements) == 0 {
+		return &SingleLinkList{}
+	}
+
+	singleLinkList := &SingleLinkList{}
+	for index, elementValue := range elements {
+		if index == 0 {
+			singleLinkList.first = NewElement(elementValue)
+			singleLinkList.last = singleLinkList.first
+		} else {
+			singleLinkList.last.next = NewElement(elementValue)
+			singleLinkList.last = singleLinkList.last.next
+		}
+	}
+	singleLinkList.length = len(elements)
+
+	return singleLinkList
+}
+
+// Traverse: 遍历单链表
+func (l SingleLinkList) Traverse() {
+	if l.first == nil && l.length == 0 {
+		return
+	}
+
+	listElementsStr := "list: ["
+	for element := l.first; element != nil; element = element.next {
+		listElementsStr += fmt.Sprintf("%v, ", element.value)
+	}
+	listElementsStr += "]"
+	println(listElementsStr)
+}
+
+// Append: 在表尾添加节点
+func (l *SingleLinkList) Append(elementValue interface{}) {
+	newElement := NewElement(elementValue)
+
+	if l.IsEmpty() {
+		l.first = newElement
+		l.last = newElement
+		l.length++
+
+		return
+	}
+
+	l.last.next = &element{
+		value: elementValue,
+		next: nil,
+	}
+	l.last = l.last.next
+	l.length++
+}
+
+// Prepend: 在表头添加元素
+func (l *SingleLinkList) Prepend(elementValue interface{}) {
+	newElement := NewElement(elementValue)
+
+	if l.IsEmpty() {
+		l.first = newElement
+		l.last = newElement
+		l.length++
+		return
+	}
+
+	newElement.next = l.first
+	l.first = newElement
+	l.length++
+}
+
+// BulkAppend: 批量在表尾添加元素
+func (l *SingleLinkList) BulkAppend(elementValues ...interface{}) {
+	if l.IsEmpty() {
+		newList := New(elementValues...)
+		l.first = newList.first
+		l.last = newList.last
+		l.length = newList.length
+
+		return
+	}
+
+	for _, elementValue := range elementValues {
+		l.Append(elementValue)
+	}
+}
+
+// Insert: 在表中的指定位置添加元素
+func (l *SingleLinkList) Insert(elementValue interface{}, index int) {
+	if index < 0 {
+		return
+	}
+
+	if l.IsEmpty() {
+		if index == 0 {
+			l.Append(elementValue)
+			return
+		}
+
+		return
+	}
+
+	if index == 0 {
+		l.Prepend(elementValue)
+	} else if index == l.length {
+		l.Append(elementValue)
+	} else {
+		preElement := l.first
+		for tmpIndex := 1; tmpIndex < index; tmpIndex++ {
+			preElement = preElement.next
+		}
+
+		newElement := NewElement(elementValue)
+		newElement.next = preElement.next
+		preElement.next = newElement
+		l.length++
+	}
+}
+
+// IsEmpty: 判断单链表是否为空
+func (l SingleLinkList) IsEmpty() bool {
+	return l.length == 0
+}
+
--- arraylist/link_list/single_link_list/single_link_list_test.go
+++ arraylist/link_list/single_link_list/single_link_list_test.go
@@ -1,1 +1,99 @@
 package single_link_list
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	// 校验关键点：
+	// 1. 校验初始化空表时表的长度及第一个元素
+	// 2. 校验初始化的非空表时，表达的长度及表是否真的不为空
+
+	list := New()
+
+	if list.first != nil || list.length != 0 {
+		t.Error("New() Error: single link list without elements initialize should be empty.")
+	}
+
+	list = New(3, 1, true)
+	if list.length != 3 || list.first == list.last {
+		t.Error("New() Error: single link list with elements initialize failed.")
+	}
+	list.Traverse()
+}
+
+func TestSingleLinkList_Append(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能够正确添加元素
+	// 2. 表不为空时，能正确的添加到表尾
+	// 3. 元素添加后长度要正确
+
+	list := New()
+
+	list.Append(3)
+	if list.length != 1 || list.first.value != 3 {
+		t.Error("Append() Error: the length or the added element value error.")
+	}
+
+	list.Append(true)
+	if list.length != 2 || list.last.value != true {
+		t.Error("Append() Error: the length or the added element value error.")
+	}
+}
+
+func TestSingleLinkList_Prepend(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能正确的添加元素
+	// 2. 表不为空时，能正确添加元素到表头
+	// 3. 添加元素后，表长度要正确
+
+	list := New()
+	list.Prepend(false)
+	if list.length != 1 || list.last != list.first || list.first.value != false {
+		t.Error("Prepend() Error: add element to the head of empty list failed.")
+	}
+
+	list.Prepend("Hello, 2020")
+	if list.length != 2 || list.first.value != "Hello, 2020" {
+		t.Error("Prepend() Error: add element to the head of list failed.")
+	}
+}
+
+func TestSingleLinkList_BulkAppend(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能否正确添加元素
+	// 2.表不为空时，能否在表尾正确添加元素
+
+	list := New()
+	list.BulkAppend(3, "a", false)
+	if list.length != 3 {
+		t.Error("BulkAppend() Error: add elements to empty list failed.")
+	}
+
+	list.BulkAppend(-1, 9)
+	if list.length != 5 || list.last.value != 9 {
+		t.Error("BulkAppend() Error: add elements to list failed.")
+	}
+}
+
+func TestSingleLinkList_Insert(t *testing.T) {
+	// 校验关键点：
+	// 1. 表为空时能否正确处理插入位置为0和其他位置的情况
+	// 2. 表不为空时，能否正确处理特殊位置的插入(表头与表尾)
+	// 3. 表不为空时，能否正确处理非首尾位置的插入
+
+	list := New()
+	list.Insert(3, 9)
+	list.Insert(-1, -1)
+	list.Insert(9, 0)
+	if list.length != 1 || list.first.value != 9 {
+		t.Error("Insert() Error: add element to empty list failed.")
+	}
+
+	list.Insert("first", 0)
+	list.Insert("last", list.length)
+	list.Insert("middle", list.length/2)
+	if list.length != 4 || list.first.value != "first" || list.last.value != "last" {
+		t.Error("Insert() Error: add element to list failed.")
+	}
+}
